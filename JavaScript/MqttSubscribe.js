2247bfd6431f895f7e73689461108fd78f8cbde4
Karlo Međimorec <kmedimore20@foi.hr>
2023-03-29 13:31:22 +0200

    Deseign

--- JavaScript/MqttSubscribe.js
+++ JavaScript/MqttSubscribe.js
@@ -20,16 +20,32 @@ function startConnect(){
   }
   
   function onConnect(){
-    topicTemeperatura = "SmartFarmKarlo/Temperatura";
-    topicVlaga = "SmartFarmKarlo/Vlaga";
-    topicSvijetlo = "SmartFarmKarlo/Svijetlo";
+
+    /*Prvi senzor*/
+    topicTemeperatura1 = "SmartFarmKarlo/Temperatura1";
+    topicVlaga1 = "SmartFarmKarlo/Vlaga1";
+    topicSvijetlo1 = "SmartFarmKarlo/Svijetlo1";
+    client.subscribe(topicTemeperatura1);
+    client.subscribe(topicVlaga1);
+    client.subscribe(topicSvijetlo1);
+
+    /*Drugi senzor*/
+    topicTemeperatura2 = "SmartFarmKarlo/Temperatura2";
+    topicVlaga2 = "SmartFarmKarlo/Vlaga2";
+    topicSvijetlo2 = "SmartFarmKarlo/Svijetlo2";
+    client.subscribe(topicTemeperatura2);
+    client.subscribe(topicVlaga2);
+    client.subscribe(topicSvijetlo2);
+
+    /*Treci senzor*/
+    topicTemeperatura3 = "SmartFarmKarlo/Temperatura3";
+    topicVlaga3 = "SmartFarmKarlo/Vlaga3";
+    topicSvijetlo3 = "SmartFarmKarlo/Svijetlo3";
+    client.subscribe(topicTemeperatura3);
+    client.subscribe(topicVlaga3);
+    client.subscribe(topicSvijetlo3);
   
-    console.log("Connected to broker");
-    console.log("Subscribing to topics: " + topicTemeperatura + ", " + topicVlaga + ", " + topicSvijetlo);
-  
-    client.subscribe(topicTemeperatura);
-    client.subscribe(topicVlaga);
-    client.subscribe(topicSvijetlo);
+    console.log("Subscribed to topic");
   }
   
   
@@ -43,19 +59,55 @@ function startConnect(){
   
   function onMessageArrived(message){
     console.log("OnMessageArrived: "+message.payloadString);
-    if(message.destinationName == "SmartFarmKarlo/Temperatura"){
-      document.getElementById("temperatura").innerHTML = "<span> Temperatura: " + message.payloadString + " °C</span><br>";
+
+    /*Poruke za prvi senzor*/
+    if(message.destinationName == "SmartFarmKarlo/Temperatura1"){
+      document.getElementById("temperatura1").innerHTML = "<span> Temperatura: " + message.payloadString + " °C</span><br>";
     }
-    if (message.destinationName == "SmartFarmKarlo/Vlaga"){
-      document.getElementById("vlaga").innerHTML = "<span> Vlaga: " + message.payloadString + " %</span><br>";
+    if (message.destinationName == "SmartFarmKarlo/Vlaga1"){
+      document.getElementById("vlaga1").innerHTML = "<span> Vlaga: " + message.payloadString + " %</span><br>";
     }
-    if (message.destinationName == "SmartFarmKarlo/Svijetlo"){
-      document.getElementById("svijetlo").innerHTML = "<span> Vlaga: " + message.payloadString + " %</span><br>";
+    if (message.destinationName == "SmartFarmKarlo/Svijetlo1"){
+      document.getElementById("svijetlo1").innerHTML = "<span> Vlaga: " + message.payloadString + " %</span><br>";
       if(message.payloadString == "1"){
-        document.getElementById("svijetlo").innerHTML = "<span> Svijetlo ne radi </span><br>";
+        document.getElementById("svijetlo1").innerHTML = "<span> Svijetlo ne radi </span><br>";
       }
       if (message.payloadString == "0"){
-        document.getElementById("svijetlo").innerHTML = "<span> Svijetlo radi </span><br>";
+        document.getElementById("svijetlo1").innerHTML = "<span> Svijetlo radi </span><br>";
+      }
+    }
+
+    /*Poruke za drugi senzor*/
+    if(message.destinationName == "SmartFarmKarlo/Temperatura2"){
+      document.getElementById("temperatura2").innerHTML = "<span> Temperatura: " + message.payloadString + " °C</span><br>";
+    }
+    if (message.destinationName == "SmartFarmKarlo/Vlaga2"){
+      document.getElementById("vlaga2").innerHTML = "<span> Vlaga: " + message.payloadString + " %</span><br>";
+    }
+    if (message.destinationName == "SmartFarmKarlo/Svijetlo2"){
+      document.getElementById("svijetlo2").innerHTML = "<span> Vlaga: " + message.payloadString + " %</span><br>";
+      if(message.payloadString == "1"){
+        document.getElementById("svijetlo2").innerHTML = "<span> Svijetlo ne radi </span><br>";
+      }
+      if (message.payloadString == "0"){
+        document.getElementById("svijetlo2").innerHTML = "<span> Svijetlo radi </span><br>";
+      }
+    }
+
+    /*Poruke za treci senzor*/
+    if(message.destinationName == "SmartFarmKarlo/Temperatura3"){
+      document.getElementById("temperatura3").innerHTML = "<span> Temperatura: " + message.payloadString + " °C</span><br>";
+    }
+    if (message.destinationName == "SmartFarmKarlo/Vlaga3"){
+      document.getElementById("vlaga3").innerHTML = "<span> Vlaga: " + message.payloadString + " %</span><br>";
+    }
+    if (message.destinationName == "SmartFarmKarlo/Svijetlo3"){
+      document.getElementById("svijetlo3").innerHTML = "<span> Vlaga: " + message.payloadString + " %</span><br>";
+      if(message.payloadString == "1"){
+        document.getElementById("svijetlo3").innerHTML = "<span> Svijetlo ne radi </span><br>";
+      }
+      if (message.payloadString == "0"){
+        document.getElementById("svijetlo3").innerHTML = "<span> Svijetlo radi </span><br>";
       }
     }
   }
